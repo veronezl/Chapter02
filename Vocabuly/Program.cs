33326984cdee128c99874154469aaa70940e6eee
veronezl <lucasveronez.dev@gmail.com>
2025-03-11 16:02:01 -0300

    Estudo de int, flot, decimal e double

--- Vocabuly/Program.cs
+++ Vocabuly/Program.cs
@@ -1,37 +1,65 @@
-﻿/*
-Console.WriteLine("Hello, World!");
-WriteLine($"Computer named {Env.MachineName} says \"No.\"");
+﻿/*int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+// Check the three variables have the same value.
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+
+// Output the variable values in decimal.
+Console.WriteLine($"{decimalNotation:N0}");
+Console.WriteLine($"{binaryNotation:N0}");
+Console.WriteLine($"{hexadecimalNotation:N0}");
+
+// Output the variable values in hexadecimal.
+Console.WriteLine($"{decimalNotation:X}");
+Console.WriteLine($"{binaryNotation:X}");
+Console.WriteLine($"{hexadecimalNotation:X}");
 */
 
-using System.Reflection; // To use Assembly, TypeName, and so on.
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
 
-// Declare some unused variables using types in
-// additional assemblies to make them load too.
-System.Data.DataSet ds = new();
-HttpClient client = new();
+Console.WriteLine();
 
+// Comparando tipos double e decimal
 
-// Get the assembly that is the entry point for this app.
-Assembly? myApp = Assembly.GetEntryAssembly();
-// If the previous line returned nothing then end the app.
-if (myApp is null) return;
-// Loop through the assemblies that my app references.
-foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+Console.WriteLine("Using doubles:");
+double a = 0.1;
+double b = 0.2;
+if (a + b == 0.3)
 {
-    // Load the assembly so we can read its details.
-    Assembly a = Assembly.Load(name);
-    // Declare a variable to count the number of methods.
-    int methodCount = 0;
-    // Loop through all the types in the assembly.
-    foreach (TypeInfo t in a.DefinedTypes)
-    {
-        // Add up the counts of all the methods.
-        methodCount += t.GetMethods().Length;
-    }
-    // Output the count of types and their methods.
-    WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
-      arg0: a.DefinedTypes.Count(),
-      arg1: methodCount,
-      arg2: name.Name);
+    Console.WriteLine($"{a} + {b} equals {0.3}");
 }
+else
+{
+    Console.WriteLine($"{a} + {b} does NOT equal {0.3}");
+}
+
+
+float y = 0.1F;
+float x = 0.2F;
+if (y + x == 0.3F) // True because float is less "accurate" than double.
+{
+    Console.WriteLine($"{y} + {x} equals {0.3}");
+}
+else
+{
+    Console.WriteLine($"{y} + {x} does NOT equal {0.3}");
+}
+
+Console.WriteLine("Using decimals:");
+decimal c = 0.1M; // M suffix means a decimal literal value
+decimal d = 0.2M;
+if (c + d == 0.3M)
+{
+    Console.WriteLine($"{c} + {d} equals {0.3M}");
+}
+else
+{
+    Console.WriteLine($"{c} + {d} does NOT equal {0.3M}");
+}
+
+
 
